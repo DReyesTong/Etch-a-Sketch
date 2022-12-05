ea0f0cdab8c0c974794b52c30c251f1870c0e384
Daniel Reyes Tong <Dreyestong@gmail.com>
2022-12-05 13:02:52 -0800

    fixed error in which grid size would not change based on input

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function populateBoard(size) {
     let board = document.querySelector(".board");
-board.style.gridTemplateColumns = 'repeat($(size) , 1fr)';
-board.style.gridTemplateRows = 'repeat($(size) , 1fr)';
+    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-for (let i = 0; i<256; i++) {
-    let square = document.createElement("div");
-    square.style.backgroundColor = "blue";
-    board.insertAdjacentElement("beforeend", square);
+    for (let i = 0; i<256; i++) {
+        let square = document.createElement("div");
+        square.style.backgroundColor = "blue";
+        board.insertAdjacentElement("beforeend", square);
 
-}
+    }
 }
 
 populateBoard(16);
 
 function changeSize(input) {
-    populateBoard(input);
+    if (input >= 2 || input <= 100) {
+        populateBoard(input);
+    } else {
+        console.log("too many squares");
+    }
 }
